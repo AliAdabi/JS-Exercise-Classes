49af2840b5ae2363744ec767bc96fe14624562d4
Aliadabi <Aliadabi3780@gmail.com>
2020-03-26 15:30:51 -0700

    Did the first 3

--- index.js
+++ index.js
@@ -41,7 +41,25 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood){
+    if(this.stomach.length<10){
+      this.stomach.push(someFood)
+    }
 
+  }
+  poop(){
+      this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
+
+  
 }
 
 /*
@@ -59,7 +77,30 @@ class Person {
 */
 
 class Car {
-
+  constructor(model,milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    let max = this.milesPerGallon * this.tank;
+    if(max >= distance){
+      this.odometer += distance
+      this.tank -= distance/this.milesPerGallon;
+    }
+    else{
+      this.odometer += max;
+      this.tank=0;
+    }
+    
+  }
+  fuel(){
+    return `I ran out of fuel at ${this.odometer} miles! `
+  }
 }
 
 /*
@@ -75,7 +116,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+constructor(attributes){
+  this.name = attributes.name;
+  this.age = attributes.age;
+  this.location = attributes.location;
+}
+speak(){
+  return `Hello my name is ${this.name}, I am from ${this.location}`
+}
 }
 
 /*
